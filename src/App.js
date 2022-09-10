9fa41eaa2b4f268e357b30db7713d3f638b3f7c4
Jean Frusca <jeang96@gmail.com>
2022-09-10 00:18:54 -0300

    corrigindo mensagens e lógica da rota de login.

--- src/App.js
+++ src/App.js
@@ -5,17 +5,21 @@ import SignUp from "./SignUp/SignUp";
 import TransactionsScreen from "./TransactionsScreen/TransactionsScreen";
 import NewIncome from "./NewIncome/NewIncome";
 import NewOutcome from "./NewOutcome/NewOutcome";
+import UserTokenContext from "./contexts/UserTokenContext";
 
 export default function App() {
+	const [userToken, setUserToken] = useState("");
 	return (
 		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<Login />} />
-				<Route path="/cadastro" element={<SignUp />} />
-				<Route path="/atividades" element={<TransactionsScreen />} />
-				<Route path="/nova-entrada" element={<NewIncome />} />
-				<Route path="/nova-saida" element={<NewOutcome />} />
-			</Routes>
+			<UserTokenContext.Provider value={{ userToken, setUserToken }}>
+				<Routes>
+					<Route path="/" element={<Login />} />
+					<Route path="/cadastro" element={<SignUp />} />
+					<Route path="/atividades" element={<TransactionsScreen />} />
+					<Route path="/nova-entrada" element={<NewIncome />} />
+					<Route path="/nova-saida" element={<NewOutcome />} />
+				</Routes>
+			</UserTokenContext.Provider>
 		</BrowserRouter>
 	);
 }
--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,26 +1,62 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import Logo from "../GlobalStyles/Logo";
 import axios from "axios";
 import styled from "styled-components";
 import Form from "../GlobalStyles/Form";
 import EndFormButton from "../GlobalStyles/EndFormButton";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import UserTokenContext from "../contexts/UserTokenContext";
 
 export default function Login() {
-	const loginURL = "localhost:5000/login";
+	const loginURL = "http://localhost:5000/login";
+	const { setUserToken } = useContext(UserTokenContext);
 	const [formData, setFormData] = useState({
 		email: "",
 		password: "",
 	});
 	const [textButton, setTextButton] = useState("Entrar");
+	const navigate = useNavigate();
+
+	function handleForm(e) {
+		e.preventDefault();
+		axios
+			.post(loginURL, formData)
+			.then((result) => {
+				console.log("Requisição OK!");
+				console.log(result);
+				navigate("/atividades");
+			})
+			.catch((error) => {
+				alert(error.response.data.message);
+			});
+	}
+
+	function handleDataForm(e) {
+		setFormData({
+			...formData,
+			[e.target.name]: e.target.value,
+		});
+	}
 
 	return (
 		<>
 			<Logo />
-			<Form>
-				<input type="email" placeholder="E-mail" name="email" required />
+			<Form onSubmit={handleForm}>
+				<input
+					type="email"
+					placeholder="E-mail"
+					name="email"
+					onChange={handleDataForm}
+					required
+				/>
 
-				<input type="password" placeholder="Senha" name="password" required />
+				<input
+					type="password"
+					placeholder="Senha"
+					name="password"
+					onChange={handleDataForm}
+					required
+				/>
 
 				<button type="submit">
 					<Button>{textButton}</Button>
