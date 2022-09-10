4a69c9708e1f6a80810ac96b80e6b0f6b249f32b
Jean Frusca <jeang96@gmail.com>
2022-09-10 02:56:19 -0300

    preparando o front para receber as transações do servidor.

--- src/TransactionsScreen/TransactionsScreen.js
+++ src/TransactionsScreen/TransactionsScreen.js
@@ -1,16 +1,39 @@
-import { useContext } from "react";
+import axios from "axios";
+import { useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import UserNameContext from "../contexts/UserNameContext";
 import UserTokenContext from "../contexts/UserTokenContext";
 
 export default function TransactionsScreen() {
+	const { userToken } = useContext(UserTokenContext);
+	const transactionsURL = "http://localhost:5000/transactions";
+
+	const config = {
+		headers: {
+			Authorization: `Bearer ${userToken}`,
+		},
+	};
 	const { userName } = useContext(UserNameContext);
-	console.log(userName);
+
+	useEffect(() => {
+		axios
+			.get(transactionsURL, config)
+			.then((result) => {
+				console.log(result.data);
+			})
+			.catch((error) => {
+				console.log(error.response);
+			});
+	}, []);
+
 	return (
-		<>
+		<Container>
 			<Title userName={userName}>
 				{userName.length > 1 ? <h2>Olá, {userName}</h2> : <h2>Olá, pessoa</h2>}
+				<LogoutButton>
+					<ion-icon name="log-out-outline"></ion-icon>
+				</LogoutButton>
 			</Title>
 
 			<Registers>Não há registros de entrada ou saída</Registers>
@@ -30,17 +53,28 @@ export default function TransactionsScreen() {
 					</InOutButton>
 				</Link>
 			</Buttons>
-		</>
+		</Container>
 	);
 }
 
 // UI
+const Container = styled.div`
+	margin: 0px 20px;
+`;
+
 const Title = styled.div`
 	font-size: 26px;
 	font-weight: 700;
 	line-height: 30.52px;
 	color: #ffffff;
-	margin: 20px 18.75px;
+	margin: 20px 0px;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+`;
+
+const LogoutButton = styled.div`
+	font-size: 30px;
 `;
 
 const Registers = styled.div`
@@ -48,7 +82,7 @@ const Registers = styled.div`
 	justify-content: center;
 	align-items: center;
 	background-color: #ffffff;
-	width: 90%;
+	width: 100%;
 	height: 446px;
 	margin: 10px auto;
 	color: #868686;
@@ -64,7 +98,7 @@ const InOutButton = styled.div`
 	height: 114px;
 	background-color: #a328d6;
 	border-radius: 5px;
-	margin: 6px 15px;
+	margin-top: 6px;
 	color: #ffffff;
 	font-size: 17px;
 	font-weight: 700;
@@ -90,6 +124,7 @@ const InOutButton = styled.div`
 
 const Buttons = styled.div`
 	display: flex;
-	justify-content: center;
+	justify-content: space-between;
 	align-items: center;
+	width: 100%;
 `;
